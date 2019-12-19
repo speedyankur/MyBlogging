d99bd5f549e01fb751203e7d5954416eeb58b2ea
speedyankur <speedyankur@gmail.com>
2019-12-19 15:01:22 +0100

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -16,7 +16,7 @@ export default () => {
           <span role="img" aria-label="learned">
             ✏️
           </span>{' '}
-          #TIL <span aria-hidden="true">(Today I learned)</span>
+          <span aria-hidden="true">(Today I learned)</span>
         </div>
         <Link to={'/tutorial/'} className="c-header__button o-btn">
           Set up your own!
